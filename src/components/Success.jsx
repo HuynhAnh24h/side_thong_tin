0a28d4f0e2b3f199d2bf093f995e5cb9b4ebce29
HuynhAnh24h <anhdev24h@gmail.com>
2025-05-13 12:48:17 +0700

    Edit Success Text

--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -6,7 +6,8 @@ const Success = () => {
         <div className="w-[250px] h-[250px]">
             <img src={iconSuccess} alt="" className="w-full h-full" />
         </div>
-      <h1 className="color-[#584e33fb] font-bold text-[20px]">Cảm ơn bạn đã tham gia khảo sát</h1>
+      <h1 className="text-[#60230D] font-bold text-[20px]">Cảm ơn bạn đã tham gia khảo sát</h1>
+      <h1 className="text-[#60230D] font-bold text-[20px] text-center"> Mọi góp ý của bạn là động lực để Chanchan tiếp tục phát triển</h1>
     </div>
   )
 }

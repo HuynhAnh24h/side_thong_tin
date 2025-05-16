a7fd99c841288cd7339e4f73c4a2a90376b7017a
HuynhAnh24h <anhdev24h@gmail.com>
2025-05-16 21:12:34 +0700

    done

--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -3,8 +3,8 @@ import iconSuccess from "../assets/success.png"
 const Success = () => {
   return (
     <div className='flex flex-col justify-center items-center gap-5 h-[60vh]'>
-      <h1 className="text-[#60230D] font-bold text-[20px]">Cảm ơn bạn đã tham gia khảo sát</h1>
-      <h1 className="text-[#60230D] font-bold text-[20px] text-center"> Mọi góp ý của bạn là động lực để Chanchan tiếp tục phát triển</h1>
+      <h1 className="text-[#60230D] font-bold text-[20px]">Cảm ơn bồ đã tham gia khảo sát</h1>
+      <h1 className="text-[#60230D] font-bold text-[20px] text-center"> Mọi góp ý của bồ là động lực để Chanchan tiếp tục phát triển</h1>
     </div>
   )
 }
